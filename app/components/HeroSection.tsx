a3f619b9c5e35aa0cddc1cd3df85e21e9c710efc
Victoria McKenna <mckeva1201@gmail.com>
2024-10-29 22:46:44 -0500

    final

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -55,12 +55,12 @@ const HeroSection = () => {
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/image/toricircle.png"
-              alt="tori image"
+       <Image
+              src="/image/toricircle.jpeg"
+              alt="tori circle"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={350}
-              height={350}
+              width={632}
+              height={632}
             />
           </div>
         </motion.div>
